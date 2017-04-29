ada50b0e7af5b8c9869104bdc4ebed7e30af2d49
LauraForde <G00315765@gmit.ie>
2017-04-29 20:40:09 +0100

    Getting White Label error

--- src/main/java/com/sales/controllers/ProductCon.java
+++ src/main/java/com/sales/controllers/ProductCon.java
@@ -1,6 +1,35 @@
 package com.sales.controllers;
 
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sales.models.Product;
+import com.sales.services.ProductSer;
+
+@Controller
 public class ProductCon {
+	@Autowired ProductSer productS;
 	
+	@RequestMapping (value = "/showProducts", method = RequestMethod.GET)
+	public String showProduct(Model m) {
+
+		ArrayList<Product> products = productS.getAll();
+
+		for (Product p1 : products) {
+			System.out.println("name = " + p1.getpId() );
+		}
+
+		m.addAttribute("products", products);
+
+		return "dispProduct";
+	}
 
 }
